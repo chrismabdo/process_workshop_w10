052312ef2088c1972e797bcdcd86d28d93d89716
Chris Abdo <christophermabdo@icloud.com>
2021-02-10 17:33:55 +0000

    score method set up

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,2 +1,4 @@
 class Scrabble
+    def score(word)
+    end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -7,4 +7,7 @@ describe Scrabble do
     it 'will return an instance of itself' do
       expect(subject).to be_kind_of Scrabble
     end
+    it 'will take a method score with one argument' do
+        expect(subject).to respond_to(:score).with(1).argument
+    end
 end
