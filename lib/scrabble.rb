d64b6da8bcd886a62246831c9868136438f861a8
Chris Abdo <christophermabdo@icloud.com>
2021-02-10 17:38:26 +0000

    tested for empty and one letter string

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,4 +1,9 @@
 class Scrabble
     def score(word)
+        if word.include?("a")
+            1
+        else
+            0
+        end
     end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -4,10 +4,20 @@ describe Scrabble do
     before(:each) do
         scrabble = Scrabble.new
     end
+
     it 'will return an instance of itself' do
       expect(subject).to be_kind_of Scrabble
     end
+
     it 'will take a method score with one argument' do
-        expect(subject).to respond_to(:score).with(1).argument
+      expect(subject).to respond_to(:score).with(1).argument
+    end
+
+    it 'will return 0 for an empty string' do
+      expect(subject.score("")).to eq 0
+    end
+
+    it 'will return the correct score for a one letter word' do
+      expect(subject.score("a")).to eq 1
     end
 end
